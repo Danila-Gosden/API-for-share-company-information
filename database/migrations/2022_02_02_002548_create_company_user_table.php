7fa895c5cf470e39145ed61badc1f84a65a9d948
Danila-Gosden <danilagosden@gmail.com>
2022-02-02 02:40:48 +0200

    Create migrations and models

---
+++ database/migrations/2022_02_02_002548_create_company_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompanyUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('company_user', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->integer('company_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('company_user', function (Blueprint $table) {
+            //
+        });
+    }
+}
